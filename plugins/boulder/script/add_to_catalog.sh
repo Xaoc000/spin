be6e29c2bdadb7f9e9a29f5358a2cbdc5ed12a85
Ian <ianmathieu@gmail.com>
2019-04-18 15:07:06 -0700

    Adding catalog items dynamically

--- plugins/boulder/script/add_to_catalog.sh
+++ plugins/boulder/script/add_to_catalog.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 
+NAME=$1
+IMAGE_NAME=$2
+IMAGE_TAG=$3
+USER_ID=$4
+
+iofog-controller catalog add  --name "${NAME}" --arm-image ${IMAGE_NAME}:${IMAGE_TAG} --registry-id 1 --user-id ${USER_ID} --category "some-category"
