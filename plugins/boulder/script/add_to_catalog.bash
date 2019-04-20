31a937b1c636868d62a203636fb9f0a849d326e0
Ian <ianmathieu@gmail.com>
2019-04-20 12:44:17 -0700

    Fixing 'cannot find file' issue

--- plugins/boulder/script/add_to_catalog.bash
+++ plugins/boulder/script/add_to_catalog.bash
@@ -4,8 +4,10 @@ set -e
 
 USER_ID=$1
 
-export BOULDER=$(iofog-controller catalog add  --name "BoulderAI" --arm-image edgeworx/boulderai:latest --registry-id 1 --user-id ${USER_ID} --category "some-category" | grep "id" | awk '{print $2}')
+BOULDER=$(iofog-controller catalog add  --name "BoulderAI" --arm-image edgeworx/boulderai:latest --registry-id 1 --user-id ${USER_ID} --category "some-category" | grep "id" | awk '{print $2}')
+export BOULDER ${BOULDER}
 
-export WEB=$(iofog-controller catalog add  --name "WebService" --arm-image edgeworx/webservice:latest --registry-id 1 --user-id ${USER_ID} --category "some-category" | grep "id" | awk '{print $2}')
+WEB=$(iofog-controller catalog add  --name "WebService" --arm-image edgeworx/webservice:latest --registry-id 1 --user-id ${USER_ID} --category "some-category" | grep "id" | awk '{print $2}')
+export WEB ${WEB}
 
 CATALOG_IDS=( ${BOULDER} ${WEB} )
